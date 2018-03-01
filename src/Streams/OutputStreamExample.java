207a836ee25782d0e32965807f0389c3345975b2
Matthew <matbulat@gmail.com>
2018-03-01 18:18:37 +0000

    Added empty output

--- src/Streams/OutputStreamExample.java
+++ src/Streams/OutputStreamExample.java
@@ -7,7 +7,8 @@ import java.io.IOException;
 public class OutputStreamExample {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-
+            
+        System.out.println("Hello");
         try (FileOutputStream output = new FileOutputStream("resources/temp.dat")) {
             for (int i = 1; i <= 10; i++) {
                 output.write(i);
